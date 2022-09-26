86122b166c31a094fb00a11f8ebc968b40acecbb
johanes.christian <johanes.christian@ui.ac.id>
2022-09-26 21:10:28 +0700

    [GREEN] implement OdiUserManager

--- users/managers.py
+++ users/managers.py
@@ -4,7 +4,19 @@ from django.utils.translation import gettext_lazy as _
 
 class OdiUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
-        return None
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
     def create_superuser(self, email, password):
-        return None
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_admin = True
+        user.is_superuser = True
+        user.save()
+
+        return user
