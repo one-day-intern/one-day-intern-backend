f18a902759a85a4b6a4e5dcb5a97a78e42fe43f5
johanes.christian <johanes.christian@ui.ac.id>
2022-09-26 23:42:12 +0700

    [GREEN] implement utility functions

--- users/services/utils.py
+++ users/services/utils.py
@@ -1,20 +1,44 @@
 from datetime import datetime
+import re
+
+DATETIME_FORMAT = '%Y-%m-%d'
+email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 
 def validate_password(password) -> dict:
-    return {
-        'is_valid': False,
+    validation_result = {
+        'is_valid': True,
         'message': None
     }
+    if len(password) < 8:
+        validation_result['is_valid'] = False
+        validation_result['message'] = 'Password length must be at least 8 characters'
+    elif not any(character.isupper() for character in password):
+        validation_result['is_valid'] = False
+        validation_result['message'] = 'Password length must contain at least 1 uppercase character'
+    elif not any(character.islower() for character in password):
+        validation_result['is_valid'] = False
+        validation_result['message'] = 'Password length must contain at least 1 lowercase character'
+    elif not any(character.isdigit() for character in password):
+        validation_result['is_valid'] = False
+        validation_result['message'] = 'Password length must contain at least 1 number character'
+    return validation_result
 
 
 def validate_email(email) -> bool:
-    return False
+    return re.fullmatch(email_regex, email)
 
 
-def validate_date_format(date_str) -> bool:
-    return False
+def validate_date_format(date_text) -> bool:
+    date_text = date_text.split('T')[0]
+    try:
+        datetime.strptime(date_text, DATETIME_FORMAT)
+        return True
+    except ValueError:
+        return False
 
 
-def get_date_from_string(date_str):
-    return datetime.now()
+def get_date_from_string(date_text):
+    date_text = date_text.split('T')[0]
+    date_text_datetime = datetime.strptime(date_text, DATETIME_FORMAT)
+    return date_text_datetime.date()
