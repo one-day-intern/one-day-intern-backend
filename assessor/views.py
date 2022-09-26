1c4645bde19f487db88a2dd89a14c8d7cbdf1268
indiradr <indira.devi@ui.ac.id>
2022-09-27 02:40:40 +0700

    [REFACTOR] explicitly define authorized HTTP methods

--- assessor/views.py
+++ assessor/views.py
@@ -1,9 +1,11 @@
+from django.views.decorators.http import require_GET
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
 
 @api_view(['GET'])
+@require_GET
 @permission_classes([IsAuthenticated])
 def assessor_dashboard(request):
 
