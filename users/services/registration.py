478404d8379eaac64cf0fde49def77aaa94c2915
johanes.christian <johanes.christian@ui.ac.id>
2022-09-27 02:30:39 +0700

    [GREEN] implement company registration

--- users/services/registration.py
+++ users/services/registration.py
@@ -1,13 +1,52 @@
+from ..models import Company
+from ..exceptions.exceptions import InvalidRegistrationException
+from . import utils
+
+
 def validate_user_registration_data(request_data):
-    raise Exception('Dummy Exception')
+    email = request_data.get('email')
+    password = request_data.get('password')
+
+    if not email:
+        raise InvalidRegistrationException('Email must not be null')
+    if not utils.validate_email(email):
+        raise InvalidRegistrationException('Email is invalid')
+    if not password:
+        raise InvalidRegistrationException('Password must not be null')
+
+    validate_password_result = utils.validate_password(password)
+
+    if not (validate_password_result['is_valid']):
+        raise InvalidRegistrationException(validate_password_result['message'])
+    if password != request_data.get('confirmed_password'):
+        raise InvalidRegistrationException('Password must match password confirmation')
 
 
 def validate_user_company_registration_data(request_data):
-    raise Exception('Dummy Exception')
+    if not (company_name := request_data.get('company_name')) or len(company_name) < 3:
+        raise InvalidRegistrationException('Company name must be more than 3 characters')
+    if not (company_description := request_data.get('company_description')) or len(company_description) < 3:
+        raise InvalidRegistrationException('Company description must be more than 3 characters')
+    if not (company_address := request_data.get('company_address')) or len(company_address) < 3:
+        raise InvalidRegistrationException('Company address must be more than 3 characters')
 
 
 def save_company_from_request_data(request_data):
-    return None
+    email = request_data.get('email')
+    password = request_data.get('password')
+    company_name = request_data.get('company_name')
+    company_description = request_data.get('company_description')
+    company_address = request_data.get('company_address')
+
+    company = Company.objects.create_user(
+        email=email,
+        password=password,
+        company_name=company_name,
+        description=company_description,
+        address=company_address
+    )
+
+    return company
 
 
 def register_company(request_data):
