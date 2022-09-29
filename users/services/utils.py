628a92b2c406ec508400e9b5aa29f9d1660dde41
johanes.christian <johanes.christian@ui.ac.id>
2022-09-29 14:43:32 +0700

    [GREEN] implement sanitize_request_data function

--- users/services/utils.py
+++ users/services/utils.py
@@ -45,4 +45,6 @@ def get_date_from_string(date_text):
 
 
 def sanitize_request_data(request_data):
-    return request_data
+    for key, value in request_data.items():
+        if type(request_data[key]) == str:
+            request_data[key] = request_data[key].strip()
