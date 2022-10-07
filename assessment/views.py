7fe2dfde6d06654a53b8ff69c74c419691bed301
johanes.christian <johanes.christian@ui.ac.id>
2022-10-08 01:26:22 +0700

    [REFACTOR] add require_POST decorator on serve_create_assignment
    Added require_POST decorator to restrict request type to be only of
    type POST, according to the suggestions made by SonarCloud.

--- assessment/views.py
+++ assessment/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.http import require_POST
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .services.assessment import create_assignment
@@ -6,6 +7,7 @@ from .models import AssignmentSerializer
 import json
 
 
+@require_POST
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def serve_create_assignment(request):
