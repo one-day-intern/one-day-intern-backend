ffed172a9b38a393f2715386fafb7653bc81bb19
johanes.christian <johanes.christian@ui.ac.id>
2022-10-13 22:52:56 +0700

    [GREEN] implement get_company_or_raise_exception
    Implemented function used in authorizing request.user as company in
    accessing company related resources.

--- company/services/company.py
+++ company/services/company.py
@@ -1,3 +1,12 @@
-def get_company_or_raise_exception(user):
-    raise Exception
+from django.contrib.auth.models import User
+from assessment.exceptions.exceptions import RestrictedAccessException
+from users.models import Company
 
+
+def get_company_or_raise_exception(user: User):
+    user_email = user.email
+    found_companies = Company.objects.filter(email=user_email)
+    if len(found_companies) > 0:
+        return found_companies[0]
+    else:
+        raise RestrictedAccessException(f'User {user_email} is not a company')
