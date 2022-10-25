380b49f6f8e0eb91f1ae7723c4558fbd6fdb84b1
indiradr <indira.devi@ui.ac.id>
2022-10-25 21:45:20 +0700

    [RED] create stub and model for interactive quiz creation process
    create models for interactive quiz as well as the components such as multiple choice question and answer options, and text questions

--- assessment/models.py
+++ assessment/models.py
@@ -31,3 +31,89 @@ class AssignmentSerializer(serializers.ModelSerializer):
         ]
 
 
+class InteractiveQuiz(AssessmentTool):
+    duration_in_minutes = models.IntegerField(null=False)
+    total_points = models.IntegerField(null=False)
+
+
+class Question(models.Model):
+    TYPES_CHOICES = [
+        ('text', 'Text Question'),
+        ('multiple_choice', 'Multiple Choice Question')
+    ]
+
+    interactive_quiz = models.ForeignKey(InteractiveQuiz, related_name='questions', on_delete=models.CASCADE)
+    prompt = models.TextField(null=False)
+    points = models.IntegerField(default=0)
+    question_type = models.CharField(choices=TYPES_CHOICES, null=False, max_length=16)
+
+
+class MultipleChoiceQuestion(Question):
+    def get_answer_options(self):
+        return self.multiplechoiceansweroption_set
+
+
+class MultipleChoiceAnswerOption(models.Model):
+    question = models.ForeignKey('MultipleChoiceQuestion', related_name='questions', on_delete=models.CASCADE)
+    content = models.TextField(null=False)
+    correct = models.BooleanField(default=False)
+
+
+class TextQuestion(Question):
+    answer_key = models.TextField(null=True)
+
+
+class MultipleChoiceAnswerOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MultipleChoiceAnswerOption
+        fields = [
+            'content',
+            'correct'
+        ]
+
+
+class MultipleChoiceQuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MultipleChoiceQuestion
+        fields = [
+            'prompt',
+            'points',
+            'question_type',
+        ]
+
+
+class TextQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TextQuestion
+        fields = [
+            'prompt',
+            'points',
+            'question_type',
+            'answer_key'
+        ]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        if isinstance(instance, MultipleChoiceQuestion):
+            return MultipleChoiceQuestionSerializer(instance=instance).data
+        elif isinstance(instance, TextQuestion):
+            return TextQuestionSerializer(instance=instance).data
+
+
+class InteractiveQuizSerializer(serializers.ModelSerializer):
+    owning_company_name = serializers.ReadOnlyField(source='owning_company.company_name')
+
+    class Meta:
+        model = InteractiveQuiz
+        fields = [
+            'assessment_id',
+            'name',
+            'description',
+            'total_points',
+            'duration_in_minutes',
+            'owning_company_id',
+            'owning_company_name'
+        ]
+
--- assessment/urls.py
+++ assessment/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import serve_create_assignment
-
+from .views import serve_create_assignment, serve_create_interactive_quiz
 
 urlpatterns = [
     path('create/assignment/', serve_create_assignment),
+    path('create/interactive-quiz/', serve_create_interactive_quiz),
 ]
