e864dbe0f970081bc1c56b2c334362b6d338254c
johanes.christian <johanes.christian@ui.ac.id>
2022-10-26 00:21:44 +0700

    [GREEN] implement utility functions for creating test flow

--- assessment/services/utils.py
+++ assessment/services/utils.py
@@ -1,5 +1,6 @@
+from datetime import time, datetime
+from ..exceptions.exceptions import AssessmentToolDoesNotExist
 from ..models import AssessmentTool
-from datetime import time
 
 
 def sanitize_file_format(file_format: str):
@@ -8,9 +9,19 @@ def sanitize_file_format(file_format: str):
 
 
 def get_time_from_date_time_string(iso_datetime) -> time:
-    return None
+    iso_datetime = iso_datetime.strip('Z')
+    try:
+        datetime_: datetime = datetime.fromisoformat(iso_datetime)
+        return time(datetime_.hour, datetime_.minute)
+    except ValueError:
+        raise ValueError(f'{iso_datetime} is not a valid ISO date string')
 
 
 def get_tool_from_id(tool_id) -> AssessmentTool:
-    return None
+    found_assessment_tools = AssessmentTool.objects.filter(assessment_id=tool_id)
+
+    if found_assessment_tools:
+        return found_assessment_tools[0]
+    else:
+        raise AssessmentToolDoesNotExist(f'Assessment tool with id {tool_id} does not exist')
 
