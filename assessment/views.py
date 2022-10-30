5f572ed943b4357c9ed2bc64773c04ea2454c197
indiradr <70441226+indiradr@users.noreply.github.com>
2022-10-30 10:57:44 +0700

    PBI-01 Create Interactive Quiz (#28)
    * [RED] create test for email with same case functionality
    
    In the previous implementation, there is a hidden bug as email with same
    characters but different case can be used to register 2 different
    accounts. This commit tests the validation and controller method that
    serves the functionality for COMPANY.
    
    * [GREEN] implement functionalities to prevent duplicated email
    
    Store email in its lowercase form and perform validation to the
    normalized version of email.
    
    * [RED] create stub and model for interactive quiz creation process
    
    create models for interactive quiz as well as the components such as multiple choice question and answer options, and text questions
    
    * [RED] create tests for interactive quiz creation process
    
    * [GREEN] implement interactive quiz functionalities
    
    * [CHORES] delete migrations for users and assessment module
    
    * [CHORES] remake migrations for users and assessment module
    
    * [REFACTOR] change method to function as per codacy
    
    refactor to_representation method in models.py to function, which breaks the no-self-use (R0201) constraint
    
    * [REFACTOR] change assignment_name to name in validate_assessment_tool
    
    * [REFACTOR] change assignment_name to name in save_assignment_to_database and save_interactive_quiz_to_database
    
    * [REFACTOR] change InvalidInteractiveQuizRegistration and InvalidAssignmentRegistration
    
    refactor so that both exceptions inherit InvalidRequestException to ensure that the exceptions will be associated with a 400 response
    
    * [REFACTOR] place save_answer_option_to_database function into MultipleChoiceQuestion model
    
    refactor save_answer_option_to_database function placement for better encapsulation
    
    * [REFACTOR] remove unused import
    
    remove unused import of MultipleChoiceAnswerOption in assessment.py
    
    * [REFACTOR] add import and remove duplicate function
    
    import InteractiveQuizSerializer in views.py and remove duplicate function serve_google_register_assessee
    
    * [CHORES] merge and refactor branch staging and PBI-01-create_assignment
    
    Co-authored-by: johanes.christian <johanes.christian@ui.ac.id>
    Co-authored-by: Johanes Christian <70894988+johaneschristian@users.noreply.github.com>

--- assessment/views.py
+++ assessment/views.py
@@ -1,16 +1,16 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.views.decorators.http import require_POST, require_GET
 from django.http.response import HttpResponse, StreamingHttpResponse
+from django.views.decorators.http import require_POST, require_GET
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .services.assessment import create_assignment, create_interactive_quiz
 from one_day_intern.exceptions import AuthorizationException, RestrictedAccessException
 from users.services import utils as user_utils
-from .services.assessment import create_assignment
 from .services.test_flow import create_test_flow
 from .services.assessment_event import create_assessment_event, add_assessment_event_participation
 from .services.assessment_event_attempt import subscribe_to_assessment_flow
-from .models import AssignmentSerializer, TestFlowSerializer, AssessmentEventSerializer
+from .models import AssignmentSerializer, TestFlowSerializer, AssessmentEventSerializer, InteractiveQuizSerializer
 import json
 
 
@@ -28,6 +28,23 @@ def serve_create_assignment(request):
     request_data = json.loads(request.body.decode('utf-8'))
     assignment = create_assignment(request_data, request.user)
     response_data = AssignmentSerializer(assignment).data
+    return Response(data=response_data)
+
+
+@require_POST
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def serve_create_interactive_quiz(request):
+    """
+    request_data must contain
+    name (string),
+    description (string),
+    duration_in_minutes (integer),
+    total_points (integer)
+    """
+    request_data = json.loads(request.body.decode('utf-8'))
+    interactive_quiz = create_interactive_quiz(request_data, request.user)
+    response_data = InteractiveQuizSerializer(interactive_quiz).data
     return Response(data=response_data)
 
 
@@ -129,4 +146,5 @@ def serve_subscribe_to_assessment_flow(request):
     except Exception as exception:
         response_content = {'message': str(exception)}
         return HttpResponse(content=json.dumps(response_content), status=500)
+
 
