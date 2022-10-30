63f4b779a2a8bed8baa2a827c52cb7221b64b236
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-30 12:07:23 +0700

    [REFACTOR] refactor function from views to service
    Refactor serializeDataUsingSerializer function, initially in views.py, move it to services/assessment_tool.py

--- assessment/services/assessment_tool.py
+++ assessment/services/assessment_tool.py
@@ -1,6 +1,6 @@
 from assessment.services.assessment import get_assessor_or_raise_exception
 from users.models import Assessor, Company
-from assessment.models import AssessmentTool
+from assessment.models import AssessmentTool, AssessmentToolSerializer
 
 def get_assessment_tool_by_company(user) -> list:
     # For now, this function can be utilised by the assessors
@@ -8,3 +8,11 @@ def get_assessment_tool_by_company(user) -> list:
     company: Company = assessor.associated_company
     list_of_assessment_tools = AssessmentTool.objects.filter(owning_company=company)
     return list_of_assessment_tools
+
+def serialize_assignment_list_using_serializer(assignments):
+    serialized_assignment_list = []
+    for assignment in assignments:
+        data = AssessmentToolSerializer(assignment).data
+        data["type"] = type(assignment).__name__.lower()
+        serialized_assignment_list.append(data)
+    return data
--- assessment/views.py
+++ assessment/views.py
@@ -4,27 +4,22 @@ from django.views.decorators.http import require_POST, require_GET
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from assessment.services.assessment_tool import get_assessment_tool_by_company
+from assessment.services.assessment_tool import get_assessment_tool_by_company, serialize_assignment_list_using_serializer
 from .services.assessment import create_assignment, create_interactive_quiz
 from one_day_intern.exceptions import AuthorizationException, RestrictedAccessException
 from users.services import utils as user_utils
 from .services.test_flow import create_test_flow
 from .services.assessment_event import create_assessment_event, add_assessment_event_participation
 from .services.assessment_event_attempt import subscribe_to_assessment_flow
-from .models import AssessmentToolSerializer, AssignmentSerializer, TestFlowSerializer, AssessmentEventSerializer, InteractiveQuizSerializer
+from .models import AssignmentSerializer, TestFlowSerializer, AssessmentEventSerializer, InteractiveQuizSerializer
 import json
-
-def serializeDataUsingSerializer(assignment):
-    data = AssessmentToolSerializer(assignment).data
-    data["type"] = type(assignment).__name__.lower()
-    return data
 
 @require_GET
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def serve_get_assessment_tool(request):
     assignments = get_assessment_tool_by_company(request.user)
-    response_data =[serializeDataUsingSerializer(assignment) for assignment in assignments]
+    response_data = serialize_assignment_list_using_serializer(assignments)
     return Response(data=response_data)
 
 @require_POST
