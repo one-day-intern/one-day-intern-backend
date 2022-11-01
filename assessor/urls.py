7bc1bc1956f664c17804b6add9c0cf7f3723bf5d
indiradr <indira.devi@ui.ac.id>
2022-11-01 08:35:38 +0700

    [GREEN] implement the get an assessor's active assessees feature

--- assessor/urls.py
+++ assessor/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import assessor_dashboard
-
+from .views import assessor_dashboard, serve_get_all_active_assessees
 
 urlpatterns = [
     path('dashboard/', assessor_dashboard, name='assessor_dashboard'),
+    path('assessee-list/', serve_get_all_active_assessees, name='assessee_list'),
 ]
