1242966fbfbaf0f5e7e4c2a57e7a0bfe1d61e1d4
johanes.christian <johanes.christian@ui.ac.id>
2022-11-01 11:36:38 +0700

    [GREEN] implement all_assessment_events function

--- assessee/services/assessee_assessment_events.py
+++ assessee/services/assessee_assessment_events.py
@@ -2,5 +2,8 @@ from users.models import Assessee
 
 
 def all_assessment_events(assessee: Assessee):
-    return None
-
+    assessment_event_participation_of_user = assessee.assessmenteventparticipation_set.all()
+    assessment_events = [
+        event_participation.assessment_event for event_participation in assessment_event_participation_of_user
+    ]
+    return assessment_events
