92f6d4dfb43384ccff7c12a97b5e7d9598bda487
johanes.christian <johanes.christian@ui.ac.id>
2022-11-05 18:11:37 +0700

    [CHORES] fix google_storage_credentials function
    Add an if statement to prevent error when environment variable is not
    present.

--- assessment/services/google_storage.py
+++ assessment/services/google_storage.py
@@ -4,11 +4,13 @@ import os
 
 def setup_google_storage_credentials():
     google_application_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIAL_VALUES')
-    google_application_credentials = google_application_credentials.replace('\\\\', '\\')
-    with open('storage-credentials.json', mode='w') as google_storage_credential_file:
-        google_storage_credential_file.write(google_application_credentials)
+    if google_application_credentials:
+        google_application_credentials = google_application_credentials.replace('\\\\', '\\')
 
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'storage-credentials.json'
+        with open('storage-credentials.json', mode='w') as google_storage_credential_file:
+            google_storage_credential_file.write(google_application_credentials)
+
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'storage-credentials.json'
 
 
 def upload_file_to_google_bucket(destination_file_name, bucket_name, file):
