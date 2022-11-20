bee631f2e7fd614c9811cdc286988766be0dcdf5
johanes.christian <johanes.christian@ui.ac.id>
2022-11-20 17:47:14 +0700

    [GREEN] implement verify_password

--- users/services/login.py
+++ users/services/login.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+from one_day_intern.exceptions import InvalidLoginCredentialsException
 
 
 def verify_password(user: User, password: str):
-    raise Exception
+    if not user.check_password(password):
+        raise InvalidLoginCredentialsException(f'Password for {user.email} is invalid')
