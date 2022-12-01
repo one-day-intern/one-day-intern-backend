9ff1ec7f4ed9062c505f70beffc5d4a5328793d0
johanes.christian <johanes.christian@ui.ac.id>
2022-12-01 21:46:27 +0700

    [GREEN] implement validate_grade_assessment_tool_request

--- assessment/services/grading.py
+++ assessment/services/grading.py
@@ -1,2 +1,10 @@
+from one_day_intern.exceptions import InvalidRequestException
+
+
 def validate_grade_assessment_tool_request(request_data):
-    pass
+    if request_data.get('tool-attempt-id') is None:
+        raise InvalidRequestException('Tool attempt id must exist')
+    if request_data.get('grade') is not None and not isinstance(request_data.get('grade'), (float, int)):
+        raise InvalidRequestException('Grade must be an integer or a floating point number')
+    if request_data.get('note') is not None and not isinstance(request_data.get('note'), str):
+        raise InvalidRequestException('Note must be a string')
